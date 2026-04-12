7a200b9e2033aa01047a0ea1ad4c4fcad588e869
ZhouHan20 <3450014365@qq.com>
2026-04-12 21:17:05 +0800

    Site updated: 2026-04-12 21:17:05

--- js/git_fix.js
+++ js/git_fix.js
@@ -7,10 +7,10 @@ function createGit() {
         
         // 这行代码不求 API，直接去拉取你的实时贡献图图片
         d.innerHTML = `
-            <div style="font-weight:bold;margin:10px 0;font-size:14px;">📊 GitHub 贡献统计</div>
+            <div style="font-weight:bold;margin:10px 0;font-size:14px;"> GitHub 贡献统计</div>
             <img src="https://ghchart.rshah.org/409ba5/ZhouBaoChuan049" 
                  style="width:100%;height:auto;padding:10px;display:block;" 
-                 onerror="this.parentElement.innerHTML='🚫 统计图加载失败，请检查网络'"/>
+                 onerror="this.parentElement.innerHTML=' 统计图加载失败，请检查网络'"/>
         `;
         p.insertBefore(d, p.firstChild);
     }
